d8ea917dfa6d90f9a1ff1bf1e56b9d0985161bc7
mphjens <jensvossnack@gmail.com>
2016-04-28 17:19:08 +0200

    Fixed indent

--- spInDP/SequenceController.py
+++ spInDP/SequenceController.py
@@ -1,4 +1,4 @@
-﻿import time
+import time
 
 class SequenceController(object):
     """Executes sequences using servos."""
@@ -57,43 +57,43 @@ class SequenceController(object):
         lineNr = 0
 
         with open(filePath, 'r') as f:
-        for line in f:
-            lineNr += 1
-            words = line.split(' ')
-            #if(not hasHeader and words[0].lower() != "sequence"):
-            #    raise("Sequencefile has an invalid header, it should start with 'Sequence <sequencename>'")
-            #else:
-            #    hasHeader = True
-            #    continue
-                
-            if(words[0].lower() == "delay"):
-                if(len(words) != 2):
-                    raise NameError("No argument given for delay at line: " + str(lineNr))
-                seconds = float(words[1]) / 1000
-                
-                if(not validate):
-                    print("Will delay " + str(seconds) + " seconds")
-                    time.sleep(seconds)
-                
-            if(words[0].lower().startswith('s:')):
-                if(len(words) < 2 or len(words) > 3):
-                    raise NameError("Wrong amount of arguments for servo control: " + str(len(words)) + " at line: " + str(lineNr))
+            for line in f:
+                lineNr += 1
+                words = line.split(' ')
+                #if(not hasHeader and words[0].lower() != "sequence"):
+                #    raise("Sequencefile has an invalid header, it should start with 'Sequence <sequencename>'")
+                #else:
+                #    hasHeader = True
+                #    continue
                     
-                servoID = int(words[0].split(':')[1]);
-                coords = words[1].split(',');
-                if(len(coords) != 3):
-                    raise NameError("Wrong amount of coords: "+str(len(coords))+" at line: " + str(lineNr))
-                
-                speed = -1
-                if(len(words) == 3):
-                    speed = int(words[2])
+                if(words[0].lower() == "delay"):
+                    if(len(words) != 2):
+                        raise NameError("No argument given for delay at line: " + str(lineNr))
+                    seconds = float(words[1]) / 1000
+                    
+                    if(not validate):
+                        print("Will delay " + str(seconds) + " seconds")
+                        time.sleep(seconds)
+                    
+                if(words[0].lower().startswith('s:')):
+                    if(len(words) < 2 or len(words) > 3):
+                        raise NameError("Wrong amount of arguments for servo control: " + str(len(words)) + " at line: " + str(lineNr))
+                        
+                    servoID = int(words[0].split(':')[1]);
+                    coords = words[1].split(',');
+                    if(len(coords) != 3):
+                        raise NameError("Wrong amount of coords: "+str(len(coords))+" at line: " + str(lineNr))
+                    
+                    speed = -1
+                    if(len(words) == 3):
+                        speed = int(words[2])
 
-                if(not validate):
-                    print("Will control servo {0}, coords: {1}, speed: {2}".format(servoID, coords, speed) )
-                    s = 200
-                    if(speed > 0):
-                        s = speed
-                    
-                    self.servoController.move(servoID, int(coords[0]), s)
-                    #hier de servocontroller aanroepen met de variabelen wanneer not validate
+                    if(not validate):
+                        print("Will control servo {0}, coords: {1}, speed: {2}".format(servoID, coords, speed) )
+                        s = 200
+                        if(speed > 0):
+                            s = speed
+                        
+                        self.servoController.move(servoID, int(coords[0]), s)
+                        #hier de servocontroller aanroepen met de variabelen wanneer not validate
 
