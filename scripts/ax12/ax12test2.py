002072d92a6e824c439b5eaf22f4750e58e3218e
Henk Mollema <henkmollema@gmail.com>
2016-05-09 13:45:32 +0200

    Add ax12 test scripts

--- scripts/ax12/ax12test2.py
+++ scripts/ax12/ax12test2.py
@@ -5,7 +5,7 @@ ax = Ax12()
 
 print("Start moving AX12")
 
-ax.moveSpeed(10, 512, 200)
-time.sleep(0.3)
-print("New pos: " + str(ax.readPosition(10)))
-print("Temp: " + str(ax.readTemperature(10)))
+ax.moveSpeed(1, 256, 200)
+time.sleep(0.5)
+print("New pos: " + str(ax.readPosition(1)))
+print("Temp: " + str(ax.readTemperature(1)))
