63aec660bec4f4cabf506ad5eb4b3451d39d808d
mphjens <jensvossnack@gmail.com>
2016-05-09 14:17:55 +0200

    Added IK code
    Also added l:{legID} syntax in sequence files.
    use: l:{legID} {x,y,z} {speed}

--- spInDP/SequenceController.py
+++ spInDP/SequenceController.py
@@ -1,7 +1,18 @@
 import time
+from spInDP.LegVector import LegVector
+
+
 
 class SequenceController(object):
     """Executes sequences using servos."""
+    
+    ## Based on physical dimensions of scarJo
+    a = 11 #Femur length (cm)
+    c = 15 #Tibia (cm)
+    e = 6.85 #height (cm)
+    d = 12.24 #Horz. afstand van c tot a (cm)
+    lc = 4.107 #Lengte coxa (cm)
+    b = math.sqrt(e**2 + d**2) #diagonal (cm)
 
     def __init__(self, spider):
         self.spider = spider
@@ -55,15 +66,45 @@ class SequenceController(object):
                         if(not validate):
                             print("Will delay " + str(seconds) + " seconds")
                             time.sleep(seconds)
+                    
+                    #Control legs
+                    if(words[0].lower().startswith('l:')):
+                        if(len(words) < 2 or len(words) > 3):
+                            raise NameError("Wrong amount of arguments for servo control: " + str(len(words)) + " at line: " + str(lineNr))
+                            
+                        legID = int(words[0].split(':')[1]);
+                        coords = words[1].split(',')[0];
+                        if(len(coords) != 3):
+                            raise NameError("Wrong amount of coords: "+str(len(coords))+" (expected 3) at line: " + str(lineNr))
                         
+                        speed = -1
+                        if(len(words) == 3):
+                            speed = int(words[2])
+
+                        if(not validate):
+                            print("Will control servo {0}, coords: {1}, speed: {2}".format(servoID, coords, speed) )
+                            s = 200
+                            if(speed > 0):
+                                s = speed
+                            
+                            try:
+                                vLeg = self.getServoPos(float(coords[0]), float(coords[1]), float(coords[2]))
+                                self.servoController.move((legID - 1) * 3 + 1, vLeg.coxa, s)
+                                self.servoController.move((legID - 1) * 3 + 2, vLeg.femur, s)
+                                self.servoController.move((legID - 1) * 3 + 3, vLeg.tibia, s)
+                            except:
+                                print("Error on line: " + str(lineNr))    
+                        
+                    
+                    #Control individual servo
                     if(words[0].lower().startswith('s:')):
                         if(len(words) < 2 or len(words) > 3):
                             raise NameError("Wrong amount of arguments for servo control: " + str(len(words)) + " at line: " + str(lineNr))
                             
                         servoID = int(words[0].split(':')[1]);
-                        coords = words[1].split(',');
-                        if(len(coords) != 3):
-                            raise NameError("Wrong amount of coords: "+str(len(coords))+" at line: " + str(lineNr))
+                        coords = words[1].split(',')[0];
+                        if(len(coords) != 1):
+                            raise NameError("Wrong amount of coords: "+str(len(coords))+" (expected 1) at line: " + str(lineNr))
                         
                         speed = -1
                         if(len(words) == 3):
@@ -79,5 +120,25 @@ class SequenceController(object):
                                 self.servoController.move(servoID, int(coords[0]), s)
                             except:
                                 print("Error on line: " + str(lineNr))
-                            #hier de servocontroller aanroepen met de variabelen wanneer not validate
+                            
 
+    def getServoPos(self, x,y,z):
+        lIK = math.sqrt((self.d+self.lc+x)**2+y**2)
+        dIK = lIK - self.lc
+        bIK = sqrt((self.e+z)**2 + dIK**2)
+    
+        alphaIK = acos(bIK**2 + self.c**2 - self.a**2) / (2*bIK*self.c)
+        betaIK  = acos(self.a**2 + self.c**2 -bIK**2) / (2*self.a*self.c)
+        gammaIK = acos(self.a**2 + bIK**2 - self.c**2) / (2 * self.a * bIK)
+        thetaIK = asin(y/lIK)
+        tauIK   = atab(self.e/dIK)
+    
+        angleCoxa = thetaIK * (180/math.pi)
+        angleFemur = (alphaIK - tauIK) * (180/math.pi)
+        angleTibia = (betaIK) * (180/math.pi)
+
+        retVal = LegVector()
+        retval.coxa = 180 - angleCoxa
+        retVal.femur = 180 - angleFemur
+        retVal.tibia = 180 - angleTibia
+        return retVal
