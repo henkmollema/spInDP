b7aa0a6a053611b49a0705c2a5f6b18cfecb0421
Henk Mollema <henkmollema@gmail.com>
2016-05-11 16:08:20 +0200

    Fix femur offset and implement leg threads with a queue

--- spInDP/SequenceController.py
+++ spInDP/SequenceController.py
@@ -1,7 +1,33 @@
 import time
 import math
+import threading
+import Queue
 from spInDP.LegVector import LegVector
 
+class LegThread(threading.Thread):
+    def __init__(self, legId, sequenceController, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
+        super(LegThread, self).__init__()
+        self.target = target
+        self.name = name
+        self.legId = legId
+        self.sequenceController = sequenceController
+        return
+        
+    def run(self):
+        q = self.sequenceController.legQueue[self.legId]
+        while True:        
+            if not q.empty():                
+                vLeg = q.get()
+                print("got instr: " + str(vLeg)+ str(q.qsize()) + ", items in queue")
+                
+                self.sequenceController.servoController.move((self.legId - 1) * 3 + 1, vLeg.coxa, vLeg.coxaSpeed)
+                self.sequenceController.servoController.move((self.legId - 1) * 3 + 2, vLeg.femur, vLeg.femurSpeed)
+                self.sequenceController.servoController.move((self.legId - 1) * 3 + 3, vLeg.tibia, vLeg.tibiaSpeed)
+                
+                time.sleep(vLeg.maxExecTime)
+                
+                #time.sleep(0.001)
+        return
 
 class SequenceController(object):
     """Parses and executes sequences using servos."""
@@ -9,29 +35,51 @@ class SequenceController(object):
     # Based on physical dimensions of scarJo
     a = 11.0  # Femur length (cm)
     c = 15.0  # Tibia (cm)
-    e = 6.85  # height (cm)
+    e = 5.60  # height (cm)
     d = 12.24  # Horz. afstand van c tot a (cm)
-    lc = 6.645  # Lengte coxa (cm)
+    lc = 4.107  # Lengte coxa (cm)
     b = math.sqrt(e**2 + d**2)  # diagonal (cm)
-
+    
     def __init__(self, spider):
         self.spider = spider
         self.servoController = spider.servoController
-        self.threadMap = {
-            1: None,
-            2: None,
-            3: None,
-            4: None,
-            5: None,
-            6: None
+        self.legQueue = {
+            1: Queue.Queue(),
+            2: Queue.Queue(),
+            3: Queue.Queue(),
+            4: Queue.Queue(),
+            5: Queue.Queue(),
+            6: Queue.Queue()
         }
+        self.threadMap = {
+            1: LegThread(1, self, name = 'legthread'),
+            2: LegThread(2, self, name = 'legthread'),
+            3: LegThread(3, self, name = 'legthread'),
+            4: LegThread(4, self, name = 'legthread'),
+            5: LegThread(5, self, name = 'legthread'),
+            6: LegThread(6, self, name = 'legthread')
+        }
+        
+        self.threadMap[1].deamon = True
+        self.threadMap[2].deamon = True
+        self.threadMap[3].deamon = True
+        self.threadMap[4].deamon = True
+        self.threadMap[5].deamon = True
+        self.threadMap[6].deamon = True
+        
+        self.threadMap[1].start()
+        self.threadMap[2].start()
+        self.threadMap[3].start()
+        self.threadMap[4].start()
+        self.threadMap[5].start()
+        self.threadMap[6].start()
 
     def execute(self, sequenceName):
         print("Executing sequence: " + sequenceName)
 
         if sequenceName == "startup":
             # self.executeServosTest()
-            #self.executeWalk()
+            self.executeWalk()
             self.executeStartup()
 
     def executeServosTest(self):
@@ -40,7 +88,7 @@ class SequenceController(object):
 
     def executeWalk(self):
         print("Execute walk")
-        self.parseSequence("sequences/walk.txt", repeat=10)
+        self.parseSequence("sequences/walk.txt", repeat=5)
 
     def executeCrawl(self):
         print("Execute crawl")
@@ -120,20 +168,19 @@ class SequenceController(object):
                                 s = speed
 
                             # try:
-                                vLeg = self.getServoPos(float(coords[0]), float(
-                                    coords[1]), float(coords[2]), legID, s)
+                                vLeg = self.getServoPos(float(coords[0]), float(coords[1]), float(coords[2]), legID, s)
                                     
                                 if (vLeg is None):
                                     return
                                     
-                                self.servoController.move(
-                                    (legID - 1) * 3 + 1, vLeg.coxa, vLeg.coxaSpeed)
-                                self.servoController.move(
-                                    (legID - 1) * 3 + 2, vLeg.femur, vLeg.femurSpeed)
-                                self.servoController.move(
-                                    (legID - 1) * 3 + 3, vLeg.tibia, vLeg.tibiaSpeed)
+                                self.legQueue[legID].put(vLeg)
                                     
-                                time.sleep(vLeg.maxExecTime)
+                                #self.servoController.move((legID - 1) * 3 + 1, vLeg.coxa, vLeg.coxaSpeed)
+                                #self.servoController.move((legID - 1) * 3 + 2, vLeg.femur, vLeg.femurSpeed)
+                                #self.servoController.move((legID - 1) * 3 + 3, vLeg.tibia, vLeg.tibiaSpeed)
+                                    
+                                #print("sleeping for: " + str(vLeg.maxExecTime))
+                                #time.sleep(vLeg.maxExecTime)
                             # except:
                                 #print("Error on line: " + str(lineNr))
 
@@ -186,6 +233,9 @@ class SequenceController(object):
         18: 0.0
      }
 
+     # True if we need to get initial positions from servo
+    first = True
+
     def getServoPos(self, x, y, z, legID, speed):
         lIK = math.sqrt((self.d + self.lc + x)**2 + y**2)
         dIK = lIK - self.lc
@@ -195,30 +245,28 @@ class SequenceController(object):
         femurServoId = (legID - 1) * 3 + 2
         tibiaServoId = (legID - 1) * 3 + 3
 
-        # determine current position of servos
-        #coxaCurr = self.servoController.getPosition(coxaServoId)
-        #femurCurr = self.servoController.getPosition(femurServoId)
-        #tibiaCurr = self.servoController.getPosition(tibiaServoId)
-
         # get previous servo angles
         coxaCurr = self.servoAngleMap[coxaServoId]
         femurCurr = self.servoAngleMap[femurServoId]
         tibiaCurr = self.servoAngleMap[tibiaServoId]
         
-        #print("current positions: " + str(coxaCurr) + ", " + str(femurCurr) + ", " + str(tibiaCurr))
-
-        alphaIK = math.acos(
-            (bIK**2 + self.c**2 - self.a**2) / (2 * bIK * self.c))
-        betaIK = math.acos(
-            (self.a**2 + self.c**2 - bIK**2) / (2 * self.a * self.c))
-        gammaIK = math.acos(
-            (self.a**2 + bIK**2 - self.c**2) / (2 * self.a * bIK))
+        if (self.first is True):
+            self.first = False
+            coxaCurr = self.servoController.getPosition(coxaServoId)
+            femurCurr = self.servoController.getPosition(femurServoId)
+            tibiaCurr = self.servoController.getPosition(tibiaServoId)
+        
+        #print("current positions: " + str(coxaCurr) + ", " + str(femurCurr) + ", " + str(tibiaCurr))        
+        
+        betaIK = math.acos((self.a**2 + self.c**2 - bIK**2) / (2 * self.a * self.c))
+        gammaIK = math.acos((self.a**2 + bIK**2 - self.c**2) / (2 * self.a * bIK)) 
         thetaIK = math.asin(y / lIK)
         tauIK = math.atan((self.e + z) / dIK)
-
+        
         angleCoxa = thetaIK * (180 / math.pi)
-        angleFemur = -((gammaIK - tauIK) * (180 / math.pi))
-        angleTibia = 180 - ((betaIK) * (180 / math.pi))
+        angleFemur = -90 + ((gammaIK - tauIK) * (180 / math.pi))
+        angleTibia = 180 - ((betaIK) * (180 / math.pi))        
+        #print ("angle tibia :" +str(angleTibia) +", tauIK: " + str(tauIK))
         
         self.servoAngleMap[coxaServoId] = angleCoxa
         self.servoAngleMap[femurServoId] = angleFemur
@@ -229,8 +277,7 @@ class SequenceController(object):
         deltaTibia = abs(angleTibia - tibiaCurr)
         #print("deltas " + str(deltaCoxa) + ", " + str(deltaFemur) + ", " + str(deltaTibia))
 
-        retVal = LegVector()   
-
+        retVal = LegVector()
         maxDelta = max(deltaCoxa, deltaFemur, deltaTibia)
         
         if (maxDelta == 0):
@@ -254,10 +301,8 @@ class SequenceController(object):
 
         # optimal rpm is 114 without load
         timePerAngle = (114.0 * 360.0 / 60.0) * (speed / 1023.0)
-        #print("time per angle: " + str(timePerAngle))
         maxExecTime = maxDelta / timePerAngle  
         retVal.maxExecTime = maxExecTime
-
 
         #print("angleCoxa: " + str(angleCoxa))
         #print("angleFemur: " + str(angleFemur))
