48fb9a8c3d05e821afce46406a012381b9fdaba3
mphjens <jensvossnack@gmail.com>
2016-05-22 22:14:34 +0200

    Started implementing sequence frames in simulator

--- ScarjoSim/Assets/SpiderLeg.cs
+++ ScarjoSim/Assets/SpiderLeg.cs
@@ -1,27 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
 
-public class SpiderLeg : MonoBehaviour {
+public class SpiderLeg : MonoBehaviour
+{
+    public struct LegMovement
+    {
+        public bool valid;
+        public bool empty;
+        public float coxa, tibia, femur;
+        public float coxaSpeed, tibiaSpeed, femurSpeed;
+        public float maxExecTime;
+    }
+
+    public struct SequenceFrame
+    {
+        public Dictionary<int, LegMovement> movements;
+        public float maxMaxExecTime;
+    }
+
+    public static SequenceFrame newSequenceFrame(Dictionary<int, LegMovement> movements)
+    {
+        SequenceFrame retVal = new SequenceFrame();
+        float maxTime = 0f;
+        foreach(KeyValuePair<int, LegMovement> k in movements)
+        {
+            maxTime = Mathf.Max(maxTime, k.Value.maxExecTime);
+        }
+        retVal.maxMaxExecTime = maxTime;
+
+        Dictionary<int, LegMovement> scaledMoves = new Dictionary<int, LegMovement>();
+        foreach (KeyValuePair<int, LegMovement> mov in movements)
+        {
+            float scaleFactor = mov.Value.maxExecTime / retVal.maxMaxExecTime;
+
+            if (retVal.maxMaxExecTime == 0)
+                scaleFactor = 0;
+            
+
+            LegMovement newMov = new LegMovement();
+            newMov.coxa = mov.Value.coxa;
+            newMov.tibia = mov.Value.tibia;
+            newMov.femur = mov.Value.femur;
+
+            newMov.coxaSpeed = mov.Value.coxaSpeed * scaleFactor;
+            newMov.tibiaSpeed = mov.Value.tibiaSpeed * scaleFactor;
+            newMov.maxExecTime = mov.Value.maxExecTime * scaleFactor;
+
+            scaledMoves.Add(mov.Key, newMov);
+        }
+
+        retVal.movements = scaledMoves;
+
+        return retVal;
+    }
 
     public int legID;
     Transform Coxa, Femur, Tibia;
 
     Quaternion cTarget, fTarget, tTarget;
+    public Quaternion lastCoxa, lastFemur, lastTibia;
     float cFinish, fFinish, tFinish;
-    float cStart, fStart, tStart;
+    //float cStart, fStart, tStart;
+    float startTime;
+    public float maxExecTime;
 
-    public float anglePerSecond = (180 * 360) / 60; //114rpm at max speed in specification
+    List<LegMovement> queue = new List<LegMovement>();
+
+    public static float anglePerSecond = 33f; //114rpm at max speed in specification
     public float angleOffsetY = 0f;
+
+    bool emptyMove = false;
 
     bool cChanged, fChanged, tChanged;
 
     public bool shouldAnimate = true;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Coxa = this.transform;
         Femur = this.transform.Find("femur");
         Tibia = Femur.Find("tibia");
+
+        lastCoxa = Coxa.localRotation;
+        lastFemur = Femur.localRotation;
+        lastTibia = Tibia.localRotation;
 
         cTarget = Coxa.localRotation;
         fTarget = Femur.localRotation;
@@ -29,34 +93,79 @@ public class SpiderLeg : MonoBehaviour {
 
         angleOffsetY = this.transform.localRotation.eulerAngles.y;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!shouldAnimate) return;
 
-        if (Coxa.transform.localRotation != cTarget)
-        {
-            Quaternion newCoxa = Quaternion.Lerp(Coxa.transform.localRotation, cTarget, (Time.time - cStart) / (cFinish - cStart));
-            Coxa.transform.localRotation = newCoxa;
+        //Debug.Log("Anim: " + (cFinish - cStart));
+        //Debug.Log("Anim: " + ((Time.time - cStart) / (cFinish - cStart)));
+        //Debug.Log("Anim: " + (Time.time - startTime) );
+        float ratio = 0f;
+        if (maxExecTime == 0)
+            ratio = 1.1f;
+        else
+            ratio = ((Time.time - startTime) / maxExecTime);
 
+        if(ratio  <= 1)
+        {
+            //Debug.Log("Anim: " + ((Time.time - cStart) / (cFinish - cStart)));
+            if ( !emptyMove)
+            {
+                //Mathf.LerpAngle()
+                Quaternion newCoxa = Quaternion.Lerp(lastCoxa, cTarget, ratio);
+                Coxa.transform.localRotation = newCoxa;
+            }
+
+            if (!emptyMove)
+            {
+                Quaternion newFemur = Quaternion.Lerp(lastFemur, fTarget, ratio);
+                Femur.transform.localRotation = newFemur;
+            }
+
+            if (!emptyMove)
+            {
+                Quaternion newTibia = Quaternion.Lerp(lastTibia, tTarget, ratio);
+                Tibia.transform.localRotation = newTibia;
+            }
         }
-
-        if (Femur.transform.localRotation != fTarget)
+        else
         {
-            Quaternion newFemur = Quaternion.Lerp(Femur.transform.localRotation, fTarget, (Time.time - fStart) / (fFinish - fStart));
-            Femur.transform.localRotation = newFemur;
-        }
+            LegMovement nextMove = getNextMove();
+            if (nextMove.valid)
+            {
+                maxExecTime = nextMove.maxExecTime;
+                startTime = Time.time;
+                if(!nextMove.empty)
+                {
+                    //Debug.Log("New Move: " + nextMove.coxa + ", " + fixAngle(nextMove.femur) + ", " + nextMove.tibia + " EXECTIME: " + nextMove.maxExecTime);
+                    setCoxa(nextMove.coxa, nextMove.coxaSpeed);
+                    setFemur(nextMove.femur, nextMove.femurSpeed);
+                    setTibia(nextMove.tibia, nextMove.tibiaSpeed);
+                    emptyMove = false;
+                }
+                else{
+                    emptyMove = true;
+                }
+            }
 
-        if (Tibia.transform.localRotation != tTarget)
-        {
-
-            Quaternion newTibia = Quaternion.Lerp(Tibia.transform.localRotation, tTarget, (Time.time - tStart) / (tFinish - tStart));
-            Tibia.transform.localRotation = newTibia;
         }
 
     }
 
-    
+    public LegMovement getNextMove()
+    {
+        LegMovement retVal = new LegMovement();
+        if (queue.Count > 0)
+        {
+            retVal = queue[0];
+            retVal.valid = true;
+            queue.RemoveAt(0);
+        }
+
+        return retVal;
+    }
 
     public string getSequenceString(float speed)
     {
@@ -64,8 +173,8 @@ public class SpiderLeg : MonoBehaviour {
         int firstServo = ((legID - 1) * 3) + 1;
 
         retVal += "s:" + firstServo + " " + (int)getCoxa() + ",0,0 " + speed + Environment.NewLine;
-        retVal += "s:" + (firstServo+1) + " " + (int)getFemur() + ",0,0 " + speed + Environment.NewLine;
-        retVal += "s:" + (firstServo+2) + " " + (int)getTibia() + ",0,0 " + speed + Environment.NewLine;
+        retVal += "s:" + (firstServo + 1) + " " + (int)getFemur() + ",0,0 " + speed + Environment.NewLine;
+        retVal += "s:" + (firstServo + 2) + " " + (int)getTibia() + ",0,0 " + speed + Environment.NewLine;
 
         return retVal;
     }
@@ -85,53 +194,86 @@ public class SpiderLeg : MonoBehaviour {
         return Tibia.transform.localRotation.eulerAngles.z;
     }
 
-    public void setCoxa(float angle, float speed)
+    public void addMove(LegMovement lm)
     {
-        cTarget = Quaternion.Euler(0,angle + angleOffsetY, 0);
-        float distance = Quaternion.Angle(cTarget, Coxa.localRotation);
-        cStart = Time.time;
+        queue.Add(lm);
+    }
+
+    public bool hasMovesOnQueue()
+    {
+        return queue.Count > 0;
+    }
+
+    private float fixAngle(float a)
+    {
+        return a;
+    }
+
+    public float setCoxa(float angle, float speed)
+    {
         
-        cFinish = Time.time + (distance / (anglePerSecond * (speed / 1024f)));
+        lastCoxa = cTarget;
+        cTarget = Quaternion.Euler(0, fixAngle(angle + angleOffsetY), 0);
+        float distance = Math.Abs(Quaternion.Angle(cTarget, Coxa.localRotation));
+        
+        //cStart = Time.time;
+        //Debug.Log("distabce: " + distance);
+        cFinish = Time.time + ((distance) / (anglePerSecond * (speed / 1024f)));
+
+        return distance;
     }
 
-    public void setFemur(float angle, float speed)
+    public float setFemur(float angle, float speed)
     {
-        fTarget = Quaternion.Euler(0, 0, angle );
+        lastFemur = fTarget;
+        fTarget = Quaternion.Euler(0, 0, fixAngle(angle));
         float distance = Quaternion.Angle(fTarget, Femur.localRotation);
-        fStart = Time.time;
-        fFinish = Time.time + (distance / (anglePerSecond * (speed / 1024f)));
+        //fStart = Time.time;
+        //Debug.Log("distabce: " + ((distance * (180f / Mathf.PI)) / (anglePerSecond * (speed / 1024f))));
+        fFinish = Time.time + ((distance) / (anglePerSecond * (speed / 1024f)));
+
+        return distance;
     }
 
-    public void setTibia(float angle, float speed)
+    public float setTibia(float angle, float speed)
     {
-        tTarget = Quaternion.Euler(0, 0, angle);
-        float distance = Quaternion.Angle(cTarget, Tibia.localRotation);
-        tStart = Time.time;
 
-        tFinish = Time.time + (distance / (anglePerSecond * (speed / 1024f)));
+        lastTibia = tTarget;
+        tTarget = Quaternion.Euler(0, 0, fixAngle(angle));
+        float distance =  Quaternion.Angle(cTarget, Tibia.localRotation);
+        //tStart = Time.time;
+        //Debug.Log("distabce: " + ((distance) / (anglePerSecond * (speed / 1024f))));
+        tFinish = Time.time + (((distance) / anglePerSecond) * (speed / 1024f));
+
+        return distance;
     }
 
-    
-    public void moveByServoID(int servoID, float angle, float speed, bool allLegs) {
+
+    public void moveByServoID(int servoID, float angle, float speed, bool allLegs)
+    {
         if (!allLegs && servoID - ((legID - 1) * 3) < 0) return; //If it's not this leg we need
 
         int switchValue = servoID - ((legID - 1) * 3);
         if (allLegs) switchValue = servoID % 3;
 
+        float distance =  0f;
+        startTime = Time.time;
         switch (switchValue)
         {
             case 1:
-                setCoxa(angle, speed);
+                distance = setCoxa(angle, speed);
                 break;
 
             case 2:
-                setFemur(angle, speed);
+                distance = setFemur(angle, speed);
                 break;
 
             case 3:
-                setTibia(angle, speed);
+                distance = setTibia(angle, speed);
                 break;
         }
+
+        this.maxExecTime = Mathf.Max(this.maxExecTime, (((distance) / anglePerSecond) * (speed / 1024f)));
 
     }
 }
