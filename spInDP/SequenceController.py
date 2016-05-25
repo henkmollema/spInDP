6322e32b800c106342f2aa8e0d37c1635cd0276b
Henk Mollema <henkmollema@gmail.com>
2016-05-25 12:14:14 +0200

    Optimize find balloon behavior

--- spInDP/SequenceController.py
+++ spInDP/SequenceController.py
@@ -63,9 +63,15 @@ class SequenceController(object):
         
     def executeStepRight(self):
         return self.parseSequence("sequences/crab-walk.txt", repeat=1, speedModifier=1)
+        
+    def executeStabLeft(self):
+        return self.parseSequence("sequences/stab-left.txt", repeat=3, speedModifier=1)
+        
+    def executeStabRight(self):
+        return self.parseSequence("sequences/stab-right.txt", repeat=3, speedModifier=1)
 
     def executeStartup(self):
-        self.parseSequence("sequences/startup.txt")
+        return self.parseSequence("sequences/startup.txt")
 
     sequenceFrame = None
 
