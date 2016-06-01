9e3c1fe598df96f20df37646859ee4ca35d38cce
Henk Mollema <henkmollema@gmail.com>
2016-06-01 12:18:47 +0200

    Implement bluetooth in remote controller

---
+++ btrecv.py
@@ -0,0 +1,39 @@
+import bluetooth
+import time
+
+bd_addr = "20:16:03:30:80:85"
+port = 1
+sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((bd_addr, port))
+
+print ("Start reading from address " + bd_addr)
+
+rec = ""
+while True:
+  try:
+    rec += sock.recv(1024)
+    #time.sleep(1)
+  except:
+    print ("error reading bluetooth")
+    break
+    
+  rec_end = rec.find('\n')
+
+  if (rec_end != -1):
+    data = rec[:rec_end]
+    if (data.startswith('J:')):
+      xs = data.split(':')[1].split(',')
+      x = xs[0]
+      y = xs[1]
+      z = xs[2]
+      print "Joystick = X: " + x + ", Y: " + y + ", Z: " + z
+    elif (data.startswith("A:")):
+      xs = data.split(':')[1].split(',')
+      x = xs[0]
+      y = xs[1]
+      z = xs[2]
+      print "Accl = X: " + x + ", Y: " + y + ", Z: " + z
+
+  rec = rec[rec_end+1:]
+
+sock.close()
--- spInDP/RemoteController.py
+++ spInDP/RemoteController.py
@@ -1,18 +1,56 @@
+import bluetooth
+import time
+import threading
 from spInDP.RemoteContext import RemoteContext
-import glob
-import json
-
-RemoteControllerInstance = None
 
 class RemoteController(object):
     """Provides interaction with the physical remote controller."""
 
-    Context = RemoteContext()
+    stop = False
+    Context = None
     spider = None
+    sock = None
+    updateLoop = None
 
     def __init__(self, spider):
-        global RemoteControllerInstance
         self.spider = spider
-        RemoteControllerInstance = self
+        self.Context = RemoteContext()
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock.connect(("20:16:03:30:80:85", 1))
+        self.updateLoop = threading.Thread(target=self.updateContextLoop)
+        self.updateLoop.start()
+        
+    def updateContextLoop(self):
+        rec = ""
+        while not self.stop:
+            try:
+                rec += self.sock.recv(1024)
+                #time.sleep(1)
+            except:
+                print ("error reading bluetooth")
+                break
 
- 
+            rec_end = rec.find('\n')
+
+            if (rec_end != -1):
+                data = rec[:rec_end]
+                
+            # Read joystick position
+            if (data.startswith('J:')):
+                xs = data.split(':')[1].split(',')
+                x = xs[0]
+                y = xs[1]
+                z = xs[2]
+                self.Context.jX = x
+                self.Context.jY = y
+                self.Context.jZ = z
+                #print "Joystick = X: " + x + ", Y: " + y + ", Z: " + z
+            #elif (data.startswith("A:")):
+            #    xs = data.split(':')[1].split(',')
+            #    x = xs[0]
+            #    y = xs[1]
+            #    z = xs[2]
+                #print "Accl = X: " + x + ", Y: " + y + ", Z: " + z
+
+            rec = rec[rec_end+1:]
+            continue
