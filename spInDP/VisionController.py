b542162350d67dd3fc410510d094ec8b422de617
Erwin Damsma <dams1404@student.nhl.nl>
2016-06-13 00:52:11 +0200

    Added line detection to vision (untested)

--- spInDP/VisionController.py
+++ spInDP/VisionController.py
@@ -100,51 +100,78 @@ class Camera(object):
 class Vision:
     """Vision does all the thresholding and returns information about the largest blob aswell as a thresholded image"""
     visionController = None
-    thread = None 
     __resolution = None
-    last_access = 0
 
-    foundBlob = None
+    #Balloon variables
+    balloonThread = None
+    last_balloon_access = 0
+
+    foundRedBlob = None
     image = None
     coords = None
     size = None
     redleftCount = 0
     redRightCount = 0
+
+    #Line variables
+    lineThread = None
+    last_line_access = 0
+
+    foundLineBlob = None
+    lineCoords = None
     
     def __init__(self, visionController, resolution):
         self.visionController = visionController
         self.__resolution = resolution
-    def initialize(self):
-        if Vision.thread is None:
-            Vision.thread = threading.Thread(target=self._balloonThread)
-            Vision.thread.start()
+    def initializeBalloon(self):
+        if Vision.balloonThread is None:
+            Vision.balloonThread = threading.Thread(target=self._balloonThread)
+            Vision.balloonThread.start()
             while self.image is None:
                 time.sleep(0)
-
     def _balloonThread(self):
-        while time.time() - self.last_access < 5:
+        while time.time() - self.last_balloon_access < 5:
             frame = self.visionController.GetImage()
             frame = np.fromstring(frame, dtype=np.uint8)
             frame = cv2.imdecode(frame, 1)
-            self.foundBlob, self.image, self.coords, self.size = self.detectRed(frame)
+            self.foundRedBlob, self.image, self.coords, self.size = self.detectRed(frame)
             foundBlueBlob, blueCoords = self.detectBlue(frame)
             if foundBlueBlob:
                 if blueCoords[0] > self.coords[0]:
                     self.redleftCount += 1
                 else:
                     self.redRightCount += 1
-        Vision.thread = None
-
+        Vision.balloonThread = None
     def getBalloonValues(self):
-        Vision.last_access = time.time()
-        self.initialize()
-        return self.foundBlob, self.image, self.coords, self.size
+        Vision.last_balloon_access = time.time()
+        self.initializeBalloon()
+        return self.foundRedBlob, self.image, self.coords, self.size
     def getBalloonPos(self):
-        Vision.last_access = time.time()
-        self.initialize()
+        Vision.last_balloon_access = time.time()
+        self.initializeBalloon()
         isLeft = self.redleftCount > self.redRightCount
         return isLeft
-        
+
+    def initializeLine(self):
+        if Vision.lineThread is None:
+            Vision.lineThread = threading.Thread(target=self._lineThread)
+            Vision.lineThread.start()
+            while self.lineResult is None:
+                time.sleep(0)
+    def _lineThread(self):
+        while time.time() - self.last_line_access < 5:
+            frame = self.visionController.GetImage()
+            frame = np.fromstring(frame, dtype=np.uint8)
+            frame = cv2.imdecode(frame, 1)
+            cropFrame = frame[self.__resolution[1] - 20:self.__resolution[1], 0:self.__resolution[0]]
+            self.foundLineBlob, self.lineCoords = self.detectLine(cropFrame)
+        Vision.lineThread = None
+    def getLineValues(self):
+        print("Warning: you are using an untested function (getLineValues in vision)")
+        Vision.last_line_access = time.time()
+        self.initializeLine()
+        return self.foundLineBlob, self.lineCoords
+
     def thresholdRange(self, img, min, max):
         """Makes it possible to give a range when thresholding instead of a min or max"""
         ret,img1 = cv2.threshold(img, min, 1, cv2.THRESH_BINARY)
@@ -209,6 +236,48 @@ class Vision:
         h = self.thresholdRange(h, 100, 125) #TODO
         s = self.thresholdRange(s, 110, 250) #TODO
         v = self.thresholdRange(v, 140, 255) #TODO
+        imageBin = h * s * v
+
+        imageBin = cv2.morphologyEx(imageBin, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
+        imageBin = cv2.morphologyEx(imageBin, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9)))
+
+        imageBin = imageBin * 255
+
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByColor = True
+        params.blobColor = 255
+        params.filterByCircularity = False
+        params.filterByArea = False
+        params.filterByCircularity = False
+        params.filterByConvexity = False
+        params.filterByInertia = False
+
+        detector = cv2.SimpleBlobDetector(params)
+        keypoints = detector.detect(imageBin)
+
+        foundBlob = False
+        coords = None
+        if keypoints:
+            i = 0
+            biggest = 0
+            for p in keypoints:
+                if (p.size > keypoints[biggest].size):
+                    biggest = i
+                i += 1
+            foundBlob = True
+            coords = keypoints[biggest].pt
+            coords = (coords[0] - (self.__resolution[0] / 2), coords[1] - (self.__resolution[1] / 2))
+        else:
+            coords = (-1, -1)
+
+        return foundBlob, coords
+
+    def detectLine(self, image):
+        imagehsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(imagehsv)
+        h = self.thresholdRange(h, 0, 179) #TODO
+        s = self.thresholdRange(s, 230, 255) #TODO
+        v = self.thresholdRange(v, 230, 255) #TODO
         imageBin = h * s * v
 
         imageBin = cv2.morphologyEx(imageBin, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
