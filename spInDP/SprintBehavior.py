2288ad53d590313995c7db94823fe85ef6d32bef
Henk Mollema <henkmollema@gmail.com>
2016-06-15 11:45:59 +0200

    Implement turn run in sprint behavior

--- spInDP/SprintBehavior.py
+++ spInDP/SprintBehavior.py
@@ -6,15 +6,35 @@ from spInDP.Behavior import Behavior
 class SprintBehavior(Behavior):
     """Provides sprinting behavior using a sequence file."""
 
+    frameNr = 0
+
     def __init__(self, spider):
         """Initializes a new instance of the SprintBehavior class."""
+
         super(SprintBehavior, self).__init__(spider)
+        self.remoteContext = spider.remoteController.context
 
     def update(self):
-        jY = self.spider.remoteController.context.jY
-        if (jY < 0.3):
-            x = self.spider.sequenceController.parseSequence("sequences/run.txt",
-                                                             validate=False,
-                                                             speedModifier=4,
-                                                             repeat=1)
-            time.sleep(x)
+        jX = self.remoteContext.jX
+        jY = self.remoteContext.jY
+        jMagnitude = self.remoteContext.jMagnitude
+
+        if jMagnitude > 0.2:
+            speedModifier = jMagnitude * 2
+
+            if self.spider.sequenceController.legQueueSize() < 2:
+                execTime = self.spider.animationController.turnRun(xDirection=-jY,
+                                                                   yDirection=-jX,
+                                                                   frameNr=self.frameNr,
+                                                                   speedMod=speedModifier) - self.spider.UPDATE_SLEEP_TIME
+                time.sleep(max(execTime, 0))
+
+            self.frameNr += 1
+
+    def restrict(self, val, minval, maxval):
+        if val < minval:
+            return minval
+        elif val > maxval:
+            return maxval
+
+        return val
