bae717f4f6d19d0700274602269409ce549ee811
Henk Mollema <henkmollema@gmail.com>
2016-06-16 15:56:10 +0200

    Catch errors when waking up sensor

--- spInDP/SensorDataProvider.py
+++ spInDP/SensorDataProvider.py
@@ -43,19 +43,21 @@ class SensorDataProvider(object):
         """Initializes the SensorDataProvider."""
         # Now wake the 6050 up as it starts in sleep mode
         # TODO: Check if the device will go to sleep mode automatically, this will cause problems
-        self._bus.write_byte_data(SensorDataProvider.BUS_ADDRESS, SensorDataProvider.POWER_MGMT_1, 0)
         print ("Init sensordataprovider")
+        try:
+            self._bus.write_byte_data(SensorDataProvider.BUS_ADDRESS, SensorDataProvider.POWER_MGMT_1, 0)
+        except BaseException as ex:
+            print("Waking up sensor failed: " + str(ex))
 
     def stopMeasuring(self):
         """Stop measuring the sensors."""
-        
+
         self._shouldMeasure = False
 
     def startMeasuring(self):
         """Starts measuring the sensors."""
 
         if (not self._shouldMeasure):
-            
             self._shouldMeasure = True
             self._lastUpdate = time.time()
 
