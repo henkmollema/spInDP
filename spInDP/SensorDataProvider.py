13bcad7aef17227b5578ce7225a11143dceca3ab
Sjoerd Brattinga <sjoerd-j-b@hotmail.com>
2016-06-17 08:56:49 +0200

    Read ADC added

--- spInDP/SensorDataProvider.py
+++ spInDP/SensorDataProvider.py
@@ -6,7 +6,20 @@ import threading
 
 class SensorDataProvider(object):
     """Provides data from sensors attached to the spider."""
-
+	
+	# Create ADS1015 ADC (12-bit) instance.
+    adc = Adafruit_ADS1x15.ADS1015()	
+	# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+	# Or pick a different gain to change the range of voltages that are read:
+	#  - 2/3 = +/-6.144V
+	#  -   1 = +/-4.096V
+	#  -   2 = +/-2.048V
+	#  -   4 = +/-1.024V
+	#  -   8 = +/-0.512V
+	#  -  16 = +/-0.256V
+	# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+	GAIN = 1
+	
     # Power management registers
     POWER_MGMT_1 = 0x6b
     # POWER_MGMT_2 = 0x6c
@@ -92,6 +105,23 @@ class SensorDataProvider(object):
             self._lastUpdate = time.time()
             time.sleep(0.0032)  # Update at 30hz
 
+	def readADC(self):
+		"""Start reading ADC values"""
+		print('Reading ADS1x15 values, press Ctrl-C to quit...')
+		# Print nice channel column headers.
+		print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+		print('-' * 37)
+		# Main loop.
+		while (self._shouldMeasure):
+			# Read all the ADC channel values in a list.
+			values = [0]*4
+			for i in range(4):
+				values[i] = adc.read_adc(i, gain=GAIN)				
+			# Print the ADC values.
+			print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+			# Pause for half a second.
+			time.sleep(0.5)			
+			
     def getSmoothAccelerometer(self):
         """Gets the smoothed accelerometer value."""
 
