2096346dab7aaf31d5a64f8dd1deee1e2dae9f25
SanderJaasma <jaas1400@student.nhl.nl>
2016-06-19 16:17:17 +0200

    Dance move - graceStrafe

--- spInDP/DanceBehavior.py
+++ spInDP/DanceBehavior.py
@@ -24,6 +24,8 @@ class DanceBehavior(Behavior):
         # plie
         print("plie")
         time.sleep(self.execSequence("dance-plie.txt", repeat=1, speedModifier=1))
+        print("graceStafe")
+        time.sleep(self.graceStrafe())
 
     def startMusic(self):
         print("start music")
@@ -32,5 +34,20 @@ class DanceBehavior(Behavior):
         return self.spider.sequenceController.parseSequence("sequences/"+ animation, repeat=int(repeat), speedModifier=float(speedModifier))
 
     def graceStrafe(self):
+        totalTime = 0
         frameNr = 0
-        self.spider.animationController.
+        execTime = 0
+        for x in range(1, 28):
+            execTime = self.spider.animationController.strafeWalk(direction=60 + x * 1, frameNr=frameNr, speedMod=1, keepLeveled=False, danceYAngle=0)
+            totalTime += execTime
+            print("execTime: ", execTime)
+            time.sleep(execTime)
+            frameNr += 1
+        frameNr = 0
+        for x in range(1, 28):
+            execTime = self.spider.animationController.strafeWalk(direction=-240 - x * 1, frameNr=frameNr, speedMod=1, keepLeveled=False, danceYAngle=0)
+            totalTime += execTime
+            time.sleep(execTime)
+            frameNr += 1
+
+        return totalTime
