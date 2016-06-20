1e9d5c4d7893be35bb9c050f3b033f6d137276e8
Henk Mollema <henkmollema@gmail.com>
2016-06-20 14:38:46 +0200

    Add pre-, post- and regular stab

--- spInDP/RemoteController.py
+++ spInDP/RemoteController.py
@@ -195,14 +195,14 @@ class RemoteController(object):
                         self._spider.switchBehavior(BehaviorType.Manual)
 
                 elif mode == "mating":
-                    if action == "start":
-                        print("Start mating mode")
-                    elif action == "stop":
-                        print ("Stop mating mode")
+                    if action == "pre-stab":
+                        print("Pre stab")
+                    elif action == "post-stab":
+                        print ("Post stab")
                     elif action == "up":
                         print ("Moving spider up")
-                    elif action == "down":
-                        print ("Moving spider down")
+                    elif action == "stab":
+                        print ("Stab")
 
                     print("Mating not implemented, using walk.")
                     self._spider.switchBehavior(BehaviorType.Manual)
